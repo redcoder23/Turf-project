03cb2c42ec47ed6d3547667f4ab06ba62bfb9b72
redcoder23 <130477374+redcoder23@users.noreply.github.com>
2025-02-06 04:55:10 +0530

    Add files via upload
    API  to handle delete,post,put

---
+++ turf.js
@@ -0,0 +1,125 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const { Turf, Group, Academy, Sport, History, User }
+    = require('./models');
+
+app.use(express.json());
+
+//put -> only one   
+// insert one ,insert many
+// delete one,delete many 
+
+//  change the timings of a certain model ->  
+
+// put by id
+// app.put('/turfs/:name', async (req, res) => {
+//     try {
+//         /*  
+//             const nm=req.params;  
+//          req.params would give an object with a single property:
+//               {
+//            name: "MyTurfName"
+//            }
+//         */
+//         /* 
+//              const nm=req.params.name; 
+//         */
+//         const upd = await Turf.findOne({ name: req.params.name });
+//         if (!upd) {
+//             return res.status(500).json({ error: "turf not found" });
+//         }
+
+//         const id = upd._id;
+//         const upd2 = await Turf.findByIdAndUpdate
+//             (
+//                 id,
+//                 { $set: req.body },
+//                 {
+//                     new: true
+//                 }
+//             );   
+//             res.json(upd2);  
+//     }
+//     catch (error) {
+//         res.status(500).json({ error: 'Couldnt find the turf' });
+//     }
+// });   
+
+//put 
+//   /* 
+//                          TRY AND USE MIDDLE WARE ,TRY TO USE THE GENERAL APPROACH 
+//
+//  * /
+app.put('/turfs/:name', async (req, res) => {
+    try {
+        const up = await Turf.findOneAndUpdate(
+
+            { name: req.params.name },   //find by 
+            { $set: req.body },    // changes the body
+            { new: true }  // returns true if the document is changed 
+        );
+        if (!up) {
+            return res.status(404).json({ error: 'No such turfs could be found' });
+        }
+        res.json(up);// the changed document is returned back to the client 
+    }
+    catch (error) {
+        console.error("error updating the turf:", error);
+        res.status(500).json({ error: 'error updating the turf' });
+    };
+});
+
+app.post('/turfs', async (req, res) => {
+    try {
+        const newturf = new Turf({
+            name: res.body.name,
+            address: res.body.address,
+            isOpen: res.body.isOpen,
+            timings: res.body.timings,
+            sports: res.body.sports,
+            ownerName: res.body.ownerName,
+            contact: res.body.contact,
+            img: res.body.img
+        });
+        const savedturf = await (newturf.save());
+        res.status(201).json({ savedturf });
+    }
+    catch (error) {
+        console.error('error creating the turf', error);
+        res.status(500).json({ error: 'error creating the tuf' });
+    }
+});
+
+app.delete('/turfs/:name', async (req, res) => {
+    try {
+        const deletedturf = await Turf.findOneAndDelete
+            ({ name: req.params.name })
+        if (!deletedturf) {
+            return res.status(404).json({ error: 'file doesnt exist' });
+        }
+        res.json(({ message: 'turf deleted successfully', deletedtur }));
+    }
+    catch (error) {
+        console.error('error deleting turf:', error);
+        res.status(500).json({ error: 'error deleting the turf' });
+    }
+
+}
+);
+
+app.get('/turfs/:name', async (req, res) => {
+       try  
+    {
+            const turfname=await Turf.findOne({name:req.params.name}); 
+          if(!turfname) 
+          {
+            res.status(404).json({error:'No such turfs exsist'}); 
+          } 
+          res.json({message:'found the turf',turfname}); 
+    }
+      catch (error) {
+            console.error('error cant find the turf',error);
+            res.status(500).json({error:'Couldnt find the turf'});
+    }
+});
